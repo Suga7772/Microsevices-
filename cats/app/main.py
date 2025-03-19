b61e990cb60804f5f5c2153bff019b0bd7b3879d
Mahrukh khan <147318456+Mahrukh-khan7171@users.noreply.github.com>
2025-03-19 16:33:33 +0500

    update ports

--- cats/app/main.py
+++ cats/app/main.py
@@ -25,4 +25,4 @@ def get_cat():
         return f"Unexpected error: {str(e)}", 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
